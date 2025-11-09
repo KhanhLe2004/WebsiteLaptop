639f32fbfdb29ec39753940471b694a829849cd3
KhanhLe2004 <kle160589@gmail.com>
2025-11-09 19:12:34 +0700

    aaaaaa

---
+++ WebsiteLaptop/WebLaptopBE/Areas/Admin/Controllers/TableProductAPIController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebLaptopBE.Data;
+
+namespace WebLaptopBE.Areas.Admin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TableProductAPIController : ControllerBase
+    {
+        private Testlaptop20Context db = new Testlaptop20Context();
+
+    }
+}
