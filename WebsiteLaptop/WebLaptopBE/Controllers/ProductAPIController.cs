a1665da218afacc54ba1857efdf88271e100d3e4
KhanhLe2004 <kle160589@gmail.com>
2025-11-09 17:09:33 +0700

    Hiển thị sản phẩm

---
+++ WebsiteLaptop/WebLaptopBE/Controllers/ProductAPIController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebLaptopBE.Data;
+
+namespace WebLaptopBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        private Testlaptop20Context db = new Testlaptop20Context();
+        
+        [HttpGet("all")]
+        public IActionResult GetAllProducts()
+        {
+            var lstSanPham = db.Products.AsNoTracking().ToList();
+            return Ok(lstSanPham);
+        }
+    }
+}
--- WebsiteLaptop/WebLaptopBE/Program.cs
+++ WebsiteLaptop/WebLaptopBE/Program.cs
@@ -2,10 +2,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // Sử dụng camelCase cho JSON (mặc định của ASP.NET Core)
+        // BrandId sẽ được serialize thành "brandId" trong JSON
+        options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+        // Không bỏ qua null values - đảm bảo BrandId (ngay cả khi null) vẫn được trả về
+        options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.Never;
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -17,6 +36,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Use CORS - must be before UseAuthorization
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
