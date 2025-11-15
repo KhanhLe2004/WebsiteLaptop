82903da68b04d99327277387a9fd49c1341a0432
nthtrang11 <nguyentrangg11022004@gmail.com>
2025-11-15 16:27:43 +0700

    add warranty page

--- WebsiteLaptop/WebLaptopFE/Controllers/HomeController.cs
+++ WebsiteLaptop/WebLaptopFE/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace WebLaptopFE.Controllers
             return View();
         }
 
+        public IActionResult Warranty()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
